c32612bb99f48fb6a54bdbe8a99e9747e89de5d9
Laurie T. Malau <laurie.malau@scout24.com>
2018-12-17 01:18:40 +0700

    Delete rows at the start of every scheduled task

--- src/main/java/com/rooma/scraper/ListingRepository.java
+++ src/main/java/com/rooma/scraper/ListingRepository.java
@@ -1,7 +1,16 @@
 package com.rooma.scraper;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+
+import javax.transaction.Transactional;
 
 public interface ListingRepository extends Repository<ListingDTO, Long> {
     ListingDTO save(ListingDTO listing);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE from ListingDTO")
+    void deleteAllBy(SourceName sourceName);
 }
--- src/main/java/com/rooma/scraper/ListingService.java
+++ src/main/java/com/rooma/scraper/ListingService.java
@@ -14,20 +14,33 @@ class ListingService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ListingService.class);
     private ListingRepository listingRepository;
 
-    @Scheduled(initialDelay = 2000, fixedDelay = 1000)
+    @Scheduled(initialDelay = 2000, fixedDelay = 10000)
     void fetchListingsJob() {
-        Source craigslistJob = SourceFactory.create("craigslist");
-        LOGGER.info("Starting craigslist scheduled job");
-        List<ListingDTO> craigslistListings = craigslistJob.fetch("https://berlin.craigslist.de/search/apa?lang=en&cc=gb");
+        List<ListingDTO> craigslistListings = startFetching();
+        deleteAllRows();
 
         for (ListingDTO listing : craigslistListings) {
-            try {
-                listingRepository.save(listing);
-                LOGGER.info("Saved listing {}", listing.getTitle());
-            } catch (Exception e) {
-                LOGGER.error("Unable to save {} due to {}", listing.getTitle(), e.getMessage());
-            }
+            saveNewListings(listing);
         }
+    }
 
+    private void saveNewListings(ListingDTO listing) {
+        try {
+            listingRepository.save(listing);
+            LOGGER.info("Saved listing {}", listing.getTitle());
+        } catch (Exception e) {
+            LOGGER.error("Unable to process {} due to {}", listing.getTitle(), e.getMessage());
+        }
+    }
+
+    private List<ListingDTO> startFetching() {
+        Source craigslistJob = SourceFactory.create("craigslist");
+        LOGGER.info("Starting craigslist scheduled job");
+        return craigslistJob.fetch("https://berlin.craigslist.de/search/apa?lang=en&cc=gb");
+    }
+
+    private void deleteAllRows() {
+        listingRepository.deleteAllBy(SourceName.CRAIGSLIST);
+        LOGGER.info("Deleted all rows in Craigslist table");
     }
 }
