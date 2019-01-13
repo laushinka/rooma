d7f01cf35079705c145bf31ec0a7d0c6584fafe0
Laurie <laurie.malau@gmail.com>
2019-01-13 14:49:59 +0100

    Use ObjectMapper

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ MySQL
 ```bash
 docker run --name rooma-db -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=rooma -p 3306:3306 -d mysql:5.7
 ```
+
+#Build jar file
+Run `mvn package -DskipTests=true`
+
+#Running the application
+1. Make sure there is a jar file
+2. Run `docker-compose up`
--- src/main/java/com/rooma/scraper/search/SlackMarkdownListResponse.java
+++ src/main/java/com/rooma/scraper/search/SlackMarkdownListResponse.java
@@ -1,36 +1,29 @@
 package com.rooma.scraper.search;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rooma.scraper.listing.Listing;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SlackMarkdownListResponse {
+class SlackMarkdownListResponse {
     private List<Listing> listings;
+    private ObjectMapper mapper = new ObjectMapper();
 
-    public SlackMarkdownListResponse() {
+    SlackMarkdownListResponse() {
         this.listings = new ArrayList<>();
     }
 
-    public void add(Listing listing) {
+    void add(Listing listing) {
         this.listings.add(listing);
     }
 
-    public boolean isEmpty() {
+    boolean isEmpty() {
         return this.listings.size() == 0;
     }
 
-    public String toJson() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("{ \"attachments\": [");
-        for (Listing listing : this.listings) {
-            sb.append(listing.toJson());
-            sb.append(",");
-        }
-        if (sb.length() > 0) {
-            sb.deleteCharAt(sb.length()-1);
-        }
-        sb.append("] }");
-        return sb.toString();
+    String toJson() throws JsonProcessingException {
+        return mapper.writeValueAsString("{attachments: " + listings + "}");
     }
 }
