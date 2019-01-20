09fb41801d62856b1de939209d410db1649d61a1
laushinka <laurie.malau@gmail.com>
2019-01-20 02:09:18 +0100

    Manually create caching to not lose search result

--- src/main/java/com/rooma/scraper/search/SearchController.java
+++ src/main/java/com/rooma/scraper/search/SearchController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.net.URLDecoder;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
@@ -21,10 +24,12 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 @RestController
 @RequiredArgsConstructor
 class SearchController {
+    private static final String PROMPT_QUESTION_ID = "prompt_question_id";
     private static final Logger LOGGER = LoggerFactory.getLogger(SearchController.class);
     private final ListingRepository listingRepository;
     private final SearchFilterRepository searchFilterRepository;
     private ObjectMapper objectMapper = new ObjectMapper();
+    private Map<String, SlackMarkdownListResponse> cache = new HashMap<>();
 
     @RequestMapping(
             value = "/district",
@@ -56,7 +61,9 @@ class SearchController {
         }
 
         if (!response.isEmpty()) {
-            return ResponseEntity.ok(response.toJson(searchFilter));
+            this.cache.put(searchFilter.toString(), response);
+            String prompt = objectMapper.writeValueAsString(getQuestionPrompt(searchFilter));
+            return ResponseEntity.ok(response.toJson(searchFilter, prompt));
         } else {
             return ResponseEntity.ok("No listings are found for your search criteria :cry:");
         }
@@ -69,7 +76,7 @@ class SearchController {
             consumes = APPLICATION_FORM_URLENCODED_VALUE
     )
     @ResponseBody
-    ResponseEntity<SearchFilter> slackSaveSearchRequest(@RequestBody String body) throws IOException {
+    ResponseEntity<?> slackSaveSearchRequest(@RequestBody String body) throws IOException {
         String decodedResponse = URLDecoder.decode(body, "UTF-8");
         String result = StringUtils.substringBetween(decodedResponse, "\"value\":\"", "\"}],\"callback_id\"");
         String tidiedResult = result.replaceAll("\\\\", "");
@@ -80,7 +87,48 @@ class SearchController {
         searchFilterRepository.save(searchFilter);
         logSearchFilter(searchFilter, body);
 
-        return ResponseEntity.ok(searchFilter);
+        SlackMarkdownListResponse response = this.cache.get(searchFilter.toString());
+        if (response == null) {
+            return ResponseEntity.ok(new SlackMarkdownListResponse());
+        } else {
+            this.cache.remove(searchFilter.toString());
+        }
+        return ResponseEntity.ok(response.toJson(searchFilter, ""));
+    }
+
+
+
+    private QuestionPromptToSaveSearch getQuestionPrompt(SearchFilter filter) throws JsonProcessingException {
+        return QuestionPromptToSaveSearch.builder()
+                .fallback("")
+                .title("Would you like to save your query?")
+                .text("We can send you notifications when there are new apartments :)")
+                .callback_id(PROMPT_QUESTION_ID)
+                .actions(getActions(filter))
+                .build();
+    }
+
+    private List<SlackAction> getActions(SearchFilter filter) throws JsonProcessingException {
+        List<SlackAction> slackActions = new ArrayList<>();
+
+        SlackAction saveAction = SlackAction.builder()
+                .name("Save")
+                .text("Yes")
+                .type("button")
+                .value(objectMapper.writeValueAsString(filter))
+                .build();
+
+        SlackAction doNotSaveAction = SlackAction.builder()
+                .name("No save")
+                .text("No")
+                .type("button")
+                .value("Nothing")
+                .build();
+
+        slackActions.add(saveAction);
+        slackActions.add(doNotSaveAction);
+
+        return slackActions;
     }
 
     private void logSearchFilter(SearchFilter searchFilter, String body) {
