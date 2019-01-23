b18ec2bf83fa524b7500a6b9b015a278c1c6b2ba
Laurie T. Malau <laurie.malau@scout24.com>
2019-01-23 19:09:56 +0100

    I don't know what I'm doing

--- src/main/java/com/rooma/scraper/listing/ListingFinder.java
+++ src/main/java/com/rooma/scraper/listing/ListingFinder.java
@@ -1,5 +1,7 @@
 package com.rooma.scraper.listing;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.rooma.scraper.search.SearchFilter;
 import com.rooma.scraper.search.SearchFilterRepository;
 import lombok.AllArgsConstructor;
@@ -16,25 +18,28 @@ public class ListingFinder {
     private static final Logger LOGGER = LoggerFactory.getLogger(ListingFinder.class);
     private ListingRepository listingRepository;
     private SearchFilterRepository searchFilterRepository;
-    private int processed;
 
-    @Scheduled(initialDelay = 3000, cron = "0 */12 * * *")
+    @Scheduled(initialDelay = 3000, fixedDelay = 120000)
     void loadListingsJob() {
         try {
             checkAgainstAllSavedFilters();
-            LOGGER.info("Found listings {}", processed);
+            LOGGER.info("Found listings {}");
         } catch (Exception e) {
             LOGGER.info("Could not load listings {}", e.getMessage());
         }
     }
 
-    private void checkAgainstAllSavedFilters() {
+    private void checkAgainstAllSavedFilters() throws UnirestException {
+        int processed = 0;
         List<SearchFilter> all = searchFilterRepository.findAll();
         for (SearchFilter filter : all) {
             List<Listing> newResults = getFilterResults(filter);
             if (newResults != null && newResults.size() > 0) {
-                processFilters(newResults, filter.getSlackUserId());
+                processFilters();
                 processed ++;
+                LOGGER.info("Call made to Slack with number of processed {}", processed);
+            } else {
+                LOGGER.info("No new listings yet");
             }
         }
     }
@@ -47,7 +52,21 @@ public class ListingFinder {
                 filter.getMinSize());
     }
 
-    private void processFilters(List<Listing> newResults, String slackUserId) {
-        // send to slackUserId
+    private void processFilters() throws UnirestException {
+        Unirest.post("https://slack.com/api/chat.postMessage")
+                .header("accept", "application/json")
+                .body("{\n" +
+                        "    \"ok\": true,\n" +
+                        "    \"channel\": \"DF1NHMYJD\",\n" +
+                        "    \"ts\": \"1548163280.000100\",\n" +
+                        "    \"message\": {\n" +
+                        "        \"bot_id\": \"BFJMYMT25\",\n" +
+                        "        \"type\": \"message\",\n" +
+                        "        \"text\": \"Another try\",\n" +
+                        "        \"user\": \"UF25WAA8L\",\n" +
+                        "        \"ts\": \"1548163280.000100\"\n" +
+                        "    }\n" +
+                        "}")
+                .asJson();
     }
 }
